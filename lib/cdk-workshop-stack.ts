913ad7ff0fd2b524246df27f55d585955245871f
Bhopal <bhopalsinghsis@gmail.com>
2021-09-19 18:43:27 -0400

    refactor: update lambda and apigateway stack

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as apigw from '@aws-cdk/aws-apigateway'
+import {HitCounter} from './hitcounter'
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -9,11 +10,14 @@ export class CdkWorkshopStack extends cdk.Stack {
     const lambdaInstance = new lambda.Function(this, 'hello-lambda', {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambdas'),
-      handler: 'hello.handler'
+      handler: 'hitcounter.handler'
     })
 
+   const hitCounter = new HitCounter(this, 'HitCounter', {
+     downstream: lambdaInstance
+   })
    const gateway = new apigw.LambdaRestApi(this, 'Endpoint', {
-     handler: lambdaInstance
+     handler: hitCounter.handler
    })
  }
 }
--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda'
+import * as dynamoDB from '@aws-cdk/aws-dynamodb';
 
 export interface IHitCounterProps {
     downstream: lambda.Function;
@@ -7,9 +8,28 @@ export interface IHitCounterProps {
 
 export class HitCounter extends cdk.Construct {
 
+    public readonly handler: lambda.Function
+
     constructor(scope: cdk.Construct, id: string, props:IHitCounterProps) {
         super(scope, id);
 
-        //TODO 
+        const dynamo = new dynamoDB.Table(this, 'Hits', {
+            partitionKey: { name: 'path', type: dynamoDB.AttributeType.STRING }
+        })
+
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            code: lambda.Code.fromAsset('lambdas'),
+            handler: 'hitcounter.handler',
+            environment: { 
+                HITS_COUNTER_TABLE_NAME: dynamo.tableName,
+                DOWN_STREAM_FUNCTION_NAME: props.downstream.functionName
+            }
+        })
+
+        dynamo.grantReadWriteData(this.handler)
+
+        props.downstream.grantInvoke(this.handler);
+
     }
 }
