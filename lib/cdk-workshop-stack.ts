7687fbb8047b7d00a9ad852a1add1d68691820d9
Bhopal <bhopalsinghsis@gmail.com>
2021-09-19 15:17:48 -0400

    feat(apigateway): add apigateway construct
    Apigateway to be use Lambda hello handler
    
    Closes #Add apigateway

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigw from '@aws-cdk/aws-apigateway'
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -10,5 +11,9 @@ export class CdkWorkshopStack extends cdk.Stack {
       code: lambda.Code.fromAsset('lambdas'),
       handler: 'hello.handler'
     })
+
+   const gateway = new apigw.LambdaRestApi(this, 'Endpoint', {
+     handler: lambdaInstance
+   })
  }
 }
