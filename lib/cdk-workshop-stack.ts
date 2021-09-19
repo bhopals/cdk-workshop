3ee723ec0b019c36100965b6e22859120ee43dd4
Bhopal <bhopalsinghsis@gmail.com>
2021-09-19 14:33:35 -0400

    feat: add lambda `hello` handler
    Add handler and update the stack to construct lambda instance
    
    Task #Create Lambda Handler

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // STACK
-  }
+    const lambdaInstance = new lambda.Function(this, 'hello-lambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambdas'),
+      handler: 'hello.handler'
+    })
+ }
 }
